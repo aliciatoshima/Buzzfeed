09fca7f6b0d5adf5d23ee6bec461d50bcf272907
Jennifer Marks and Patrick Miller <patrick@>
2019-02-13 18:22:30 -0800

    Add subtract function to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// addition:
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -5,4 +6,16 @@ var add = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
+alert(result);
+
+
+// subtraction:
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
 alert(result);

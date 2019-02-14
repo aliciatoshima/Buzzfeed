60ef43a5a687ffa5efa02adb4d9ae5301639dbb9
Jennifer Marks and Patrick Miller <patrick@>
2019-02-13 18:24:49 -0800

    Add multiplication function to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// addition:
+// add:
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -9,7 +9,7 @@ var result = add(number1, number2);
 alert(result);
 
 
-// subtraction:
+// subtract:
 
 var subtract = function(number1, number2) {
   return number1 - number2;
@@ -18,4 +18,15 @@ var subtract = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = subtract(number1, number2);
+alert(result);
+
+// multipy
+
+var multipy = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multipy(number1, number2);
 alert(result);

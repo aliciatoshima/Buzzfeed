7bd584fa8596bfc6a2135a4a5bab7ed9d851de37
Alicia Toshima and Jennifer Marks <jennifer@>
2019-02-20 18:05:13 -0800

    Added new calculator

--- js/scripts.js
+++ js/scripts.js
@@ -21,41 +21,15 @@ var divide = function(number1, number2) {
 
 // User interface (or front-end) logic:
 
+// Business logic not included because it will remain the same.
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
     var result = add(number1, number2);
     $("#output").text(result);
   });
-
-//Subtraction Section:
-  $("form#subtract").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#outputsubtract").text(result);
-  });
-
-// Division:
-
-$("form#divide").submit(function(event) {
- event.preventDefault();
-  var number1 = parseInt($("#divide1").val());
-  var number2 = parseInt($("#divide2").val());
-  var result = divide(number1, number2);
-  $("#outputdivide").text(result);
-});
-
-// Multiply:
-
-$("form#multiply").submit(function(event) {
- event.preventDefault();
-  var number1 = parseInt($("#multiply1").val());
-  var number2 = parseInt($("#multiply2").val());
-  var result = multiply(number1, number2);
-  $("#outputmultiply").text(result);
-});
 });

e0cbb04aa15f779680408b8880a6c585dddcb266
Alicia Toshima and Jennifer Marks <alicia@>
2019-02-20 18:56:41 -0800

    made character repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,39 @@
 
 // Addition Section:
 
-// Business (or back-end) logic:
-
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
 
 // User interface (or front-end) logic:
 
 // Business logic not included because it will remain the same.
 
 $(document).ready(function() {
-  $("form#calculator").submit(function() {
+  $("form#character").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result;
-    if (operator === "add") {
-      result = add(number1, number2);
-    } else if (operator === "subtract") {
-      result = subtract(number1, number2);
-    } else if (operator === "multiply") {
-      result = multiply(number1, number2);
-    } else if (operator === "divide") {
-      result = divide(number1, number2);
+    var color = parseInt($("input:radio[name=color]:checked").val());
+    var movie = parseInt($("input:radio[name=movie]:checked").val());
+    var vacation = parseInt($("input:radio[name=vacation]:checked").val());
+
+    var result = color + movie + vacation
+
+    if (result < 4) {
+      answer = "Michael Scott"
+      var img = new Image();
+      img.src = "https://www.bluleadz.com/hs-fs/hubfs/Blog_pics/PrisonMike.jpeg?width=598&name=PrisonMike.jpeg";
+    } else if (result > 6) {
+      answer = "Pam"
+      var img = new Image();
+      img.src = "https://uproxx.files.wordpress.com/2016/11/pam-the-office4.jpg?quality=100&w=650";
+    } else  {
+     answer = "Dwight"
+     var img = new Image();
+     img.src = "https://www.cheatsheet.com/wp-content/uploads/2018/01/Dwight-Schrute.jpg";
     }
 
-    $("#output").text(result);
+    $("#output").text(answer);
+    //$("#output").show();
+    $("#output").append(img);
+    $("#output").show();
+
+    event.preventDefault();
   });
 });

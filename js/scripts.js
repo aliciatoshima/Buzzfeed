67fd62cc8656b53a890001c017bb756697753eb9
Alicia Toshima <toshimaalicia@gmail.com>
2019-02-24 00:08:54 -0800

    take out the return. Hope it works

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,28 @@
 $(document).ready(function() {
   $("form#Buzzfeed").submit(function(event) {
     event.preventDefault();
-    
+
     var name = $("input#name").val();
-    var flavor = $("input:radio[name=like]:checked").val();
-    var flavor = $("input:radio[name=exercise]:checked").val();
-    var flavor = $("input:radio[name=food]:checked").val();
-    var flavor = $("input:radio[name=sleep]:checked").val();
-    var flavor = $("input:radio[name=dog]:checked").val();
-
-
+    var like = $("input:radio[name=like]:checked").val();
+    var exercise = $("input:radio[name=exercise]:checked").val();
+    var food = $("input:radio[name=food]:checked").val();
+    var sleep = $("input:radio[name=sleep]:checked").val();
+    var dog = $("input:radio[name=dog]:checked").val();
 
     if (name) {
-      name
+       debugger;
+       name 
     } else {
-      name = "Kaonashi"
+        name = "Kaonashi"
     }
 
-    $("#output").text(answer);
+    $("#output").text(name);
+    // $("#output").text(like);
+    // $("#output").text(exercise);
+    // $("#output").text(food);
+    // $("#output").text(sleep);
+    // $("#output").text(dog);
+
     $("#output").show();
   });
 });

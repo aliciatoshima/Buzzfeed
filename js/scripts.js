94c72115ccd37dacb75e9ae3f836e1d92684513f
Alicia Toshima <toshimaalicia@gmail.com>
2019-02-23 23:53:09 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,25 @@
 // Business logic not included because it will remain the same.
 
 $(document).ready(function() {
-  $("form#triangle").submit(function() {
+  $("form#Buzzfeed").submit(function() {
 
-    var input1 = parseInt($("input#input1").val());
-    var input2 = parseInt($("input#input2").val());
-    var input3 = parseInt($("input#input3").val());
+    var name = $("input#name").val();
+    var flavor = $("input:radio[name=like]:checked").val();
+    var flavor = $("input:radio[name=exercise]:checked").val();
+    var flavor = $("input:radio[name=food]:checked").val();
+    var flavor = $("input:radio[name=sleep]:checked").val();
+    var flavor = $("input:radio[name=dog]:checked").val();
 
 
-    if (input1 === input2 && input2 === input3) {
-      answer = "Equilateral"
-    } else if (input1+ input2 <= input3
-      || input2+ input3 <= input2
-      || input3+ input1 <= input2 ) {
-      answer = "Not a Triangle!!!"
-    } else if ((input1 !== input2 && input2 === input3)
-    || (input1 !== input2 && input1 === input3)
-    || (input3 !== input2 && input2 === input1)
-  ) {
-    answer = "Isosceles"
+
+    if (name) {
+      name
     } else {
-      answer = "scalene"
+      name = "Kaonashi"
     }
 
     $("#output").text(answer);
-    //$("#output").show();
+    $("#output").show();
 
     event.preventDefault();
   });
